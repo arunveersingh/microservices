a5ab30a3e4dfd461bceed59f7ca83a3f2438a6f7
arunveersingh <arunveersingh9@gmail.com>
2018-11-22 12:23:42 -0600

    Spring Boot - RESTful web services project.
    Initial commit.

---
+++ restful-web-services-using-spring-boot/src/main/java/com/arunveersingh/rest/webservices/RestfulWebServicesUsingSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.arunveersingh.rest.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesUsingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesUsingSpringBootApplication.class, args);
+	}
+}
