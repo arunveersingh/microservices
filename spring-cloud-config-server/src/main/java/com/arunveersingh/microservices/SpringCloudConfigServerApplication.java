ab0a682520ed829aeb6f2d0402aa082c83d43fa4
arunveersingh <arunveersingh9@gmail.com>
2018-11-27 03:13:28 +0530

    Spring Cloud Config Server
    This project uses a git repo to pull the configuration and further makes
    them available through rest endpoints.

--- spring-cloud-config-server/src/main/java/com/arunveersingh/microservices/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/arunveersingh/microservices/SpringCloudConfigServerApplication.java
@@ -2,7 +2,15 @@ package com.arunveersingh.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+/**
+ * Notes
+ * http://localhost:8888/limits-service/qa
+ * http://localhost:8888/limits-service/default
+ * @author arunveersingh9@gmail.com
+ *
+ */
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
