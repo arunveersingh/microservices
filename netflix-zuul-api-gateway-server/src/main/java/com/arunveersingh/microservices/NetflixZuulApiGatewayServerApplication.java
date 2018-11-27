603a1c568873f4f172e5a630c756464cb9a14bfd
arunveersingh <arunveersingh9@gmail.com>
2018-11-28 04:24:46 +0530

    Setup of Netflix Zuul API Gateway Server
    Integration of Eureka Naming Server and Spring Cloud Sleuth.

---
+++ netflix-zuul-api-gateway-server/src/main/java/com/arunveersingh/microservices/NetflixZuulApiGatewayServerApplication.java
@@ -0,0 +1,29 @@
+package com.arunveersingh.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
+
+/**
+ * 1. Logging - {@link ZuulLoggingFilter}
+ * @author arunveersingh9@gmail.com
+ *
+ */
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NetflixZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+}
