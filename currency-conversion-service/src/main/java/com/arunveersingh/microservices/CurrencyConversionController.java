6cf8f193a2d44a753a699dd390989fb7af33b542
arunveersingh <arunveersingh9@gmail.com>
2018-11-28 04:18:17 +0530

    Integration of Eureka Naming Server, Zuul API Gateway, Spring Cloud Sleuth

--- currency-conversion-service/src/main/java/com/arunveersingh/microservices/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/arunveersingh/microservices/CurrencyConversionController.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,22 +13,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-
+/**
+ * This class has exposed rest end points for the currency-conversion service.
+ * This class demonstrate multiple ways of creating rest clients for
+ * communication with restful service e.g. RestTemplate, Feign, Feign + Ribbon.
+ * 
+ * @author arunveersingh9@gmail.com
+ *
+ */
 @RestController
 public class CurrencyConversionController {
 	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
-	private CurrencyExchangeServiceProxy proxy;
-	
+	private CurrencyExchangeServiceFeignProxy feignProxy;
+
 	@Autowired
-	private CurrencyExchangeServiceProxyWithRibbon proxyWithRibbon;
+	private CurrencyExchangeServiceFeignProxyWithRibbon feignProxyWithRibbon;
+
+	@Autowired
+	private CurrencyExchangeServiceFeignProxyThroughZuulWithRibbon feignProxyThroughZuulWithRibbon;
 
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
 
-		//==== Code to invoke Currency Exchange Service =======
+		// ==== Code to invoke Currency Exchange Service =======
 		Map<String, String> uriVariables = new HashMap<>();
 		uriVariables.put("from", from);
 		uriVariables.put("to", to);
@@ -36,48 +49,114 @@ public class CurrencyConversionController {
 				uriVariables);
 
 		CurrencyConversionBean entity = responseEntity.getBody();
-		//======================================================
+		// ======================================================
 
 		return new CurrencyConversionBean(entity.getId(), from, to, entity.getConversionMultiple(), quantity,
 				quantity.multiply(entity.getConversionMultiple()), entity.getPort());
 	}
+
 	/**
 	 * This method used feign rest client to invoke the restful services.
+	 * 
 	 * @param from
 	 * @param to
 	 * @param quantity
 	 * @return
-	 * @see CurrencyExchangeServiceProxy
-	 */ 
+	 * @see CurrencyExchangeServiceFeignProxy
+	 */
 	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
 
-		//==== Code to invoke Currency Exchange Service Using Feign and Ribbon =======
-		CurrencyConversionBean  entity = proxy.getCurrencyExchangeValue(from, to);
-		//============================================================================
+		// ==== Code to invoke Currency Exchange Service Using Feign and Ribbon =======
+		CurrencyConversionBean entity = feignProxy.getCurrencyExchangeValue(from, to);
+		// ============================================================================
 
 		return new CurrencyConversionBean(entity.getId(), from, to, entity.getConversionMultiple(), quantity,
 				quantity.multiply(entity.getConversionMultiple()), entity.getPort());
 	}
-	
+
 	/**
 	 * This method used feign rest client and Ribbon to invoke the restful services.
-	 * Try this method with multiple instance of currency-exchange-service to see load balancing working.
+	 * Try this method with multiple instance of currency-exchange-service to see
+	 * load balancing working.
+	 * 
 	 * @param from
 	 * @param to
 	 * @param quantity
 	 * @return
-	 * @see CurrencyExchangeServiceProxyWithRibbon
-	 * @see application.properties for currency-exchange-service.ribbon.listOfServers
-	 */ 
+	 * @see CurrencyExchangeServiceFeignProxyWithRibbon
+	 * @see application.properties for
+	 *      currency-exchange-service.ribbon.listOfServers
+	 */
 	@GetMapping("/currency-converter-feign-ribbon/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrencyFeignRibbon(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
 
-		//==== Code to invoke Currency Exchange Service Using Feign =======
-		CurrencyConversionBean  entity = proxyWithRibbon.getCurrencyExchangeValue(from, to);
-		//=================================================================
+		// ==== Code to invoke Currency Exchange Service Using Feign =======
+		CurrencyConversionBean entity = feignProxyWithRibbon.getCurrencyExchangeValue(from, to);
+		// =================================================================
+
+		return new CurrencyConversionBean(entity.getId(), from, to, entity.getConversionMultiple(), quantity,
+				quantity.multiply(entity.getConversionMultiple()), entity.getPort());
+	}
+
+	/**
+	 * This method used feign rest client and Ribbon to invoke the restful services.
+	 * Try this method with multiple instance of currency-exchange-service to see
+	 * load balancing working.
+	 * 
+	 * URI
+	 * http://localhost:8100/currency-converter-feign-ribbon-zuul/from/USD/to/INR/quantity/1000
+	 * 
+	 * @param from
+	 * @param to
+	 * @param quantity
+	 * @return
+	 * @see CurrencyExchangeServiceFeignProxyWithRibbon
+	 * @see application.properties for
+	 *      currency-exchange-service.ribbon.listOfServers
+	 * @see CurrencyExchangeServiceFeignProxyThroughZuulWithRibbon
+	 */
+	@GetMapping("/currency-converter-feign-ribbon-zuul/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrencyFeignZuulRibbon(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+
+		// ==== Code to invoke Currency Exchange Service Using Feign =======
+		CurrencyConversionBean entity = feignProxyThroughZuulWithRibbon.getCurrencyExchangeValue(from, to);
+		// =================================================================
+
+		return new CurrencyConversionBean(entity.getId(), from, to, entity.getConversionMultiple(), quantity,
+				quantity.multiply(entity.getConversionMultiple()), entity.getPort());
+	}
+
+	/**
+	 * This method used feign rest client and Ribbon to invoke the restful services.
+	 * Try this method with multiple instance of currency-exchange-service to see
+	 * load balancing working.
+	 * 
+	 * URI
+	 * http://localhost:8765/currency-conversion-service/currency-converter-feign-ribbon-zuul/from/USD/to/INR/quantity/1000
+	 * 
+	 * @param from
+	 * @param to
+	 * @param quantity
+	 * @return
+	 * @see CurrencyExchangeServiceFeignProxyWithRibbon
+	 * @see application.properties for
+	 *      currency-exchange-service.ribbon.listOfServers
+	 * @see CurrencyExchangeServiceFeignProxyThroughZuulWithRibbon
+	 */
+	@GetMapping("/currency-conversion-service/currency-converter-feign-ribbon-zuul/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrencyInvokedThroughZuulUsingFurtherFeignZuulRibbon(
+			@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+		
+
+		// ==== Code to invoke Currency Exchange Service Using Feign =======
+		CurrencyConversionBean entity = feignProxyThroughZuulWithRibbon.getCurrencyExchangeValue(from, to);
+		// =================================================================
+		
+		logger.info("{}", entity);
 
 		return new CurrencyConversionBean(entity.getId(), from, to, entity.getConversionMultiple(), quantity,
 				quantity.multiply(entity.getConversionMultiple()), entity.getPort());
--- currency-conversion-service/src/main/java/com/arunveersingh/microservices/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/arunveersingh/microservices/CurrencyConversionServiceApplication.java
@@ -2,22 +2,33 @@ package com.arunveersingh.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 /**
  * 
  * @author arunveersingh9@gmail.com
  * ==== FEIGN is used ===
- * @see CurrencyExchangeServiceProxy
+ * @see CurrencyExchangeServiceFeignProxy
  * @see CurrencyConversionController#convertCurrencyFeign(String, String, java.math.BigDecimal)
  *
  */
 @SpringBootApplication
 // Annotation is required to enable feign
 @EnableFeignClients("com.arunveersingh.microservices")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 }
